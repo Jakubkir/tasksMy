220f8065f5e8578f8d411b9b4492d72800e317d6
Jakub Kirker <jakubkir@wp.pl>
2022-01-26 20:44:30 +0100

    18.3 stworzenie repozytorium i przesłanie kodu

---
+++ src/main/java/com/crudMy/tasksMy/TasksMyApplication.java
@@ -0,0 +1,14 @@
+package com.crudMy.tasksMy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasksMyApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(TasksMyApplication.class, args);
+	}
+
+}
